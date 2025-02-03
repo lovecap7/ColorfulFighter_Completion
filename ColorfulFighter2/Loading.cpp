3333b1b061aaafc4648096e0f31f6bbf5c96d355
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2025-02-03 18:59:50 +0900

    up

--- ColorfulFighter2/Loading.cpp
+++ ColorfulFighter2/Loading.cpp
@@ -4,8 +4,18 @@
 
 Loading::Loading() :
 	m_loadingHandle(LoadGraph("./img/Loading/NowLoading.png")),
-	m_loadingPos(Game::kScreenWidth / 2, Game::kScreenHeight / 2, 0)
+	m_animCountFrame(0),
+	m_actor1{-1,8,2},
+	m_actor2{-1,8,2},
+	m_actor3{-1,8,2},
+	m_actor4{-1,8,2},
+	m_actor5{-1,8,2}
 {
+	m_actor1.handle = LoadGraph("./img/Chara/White/playerbase/walk_front_001.png");
+	m_actor2.handle = LoadGraph("./img/Chara/Red/playerbase/walk_front_001.png");
+	m_actor3.handle = LoadGraph("./img/Chara/Blue/playerbase/walk_front_001.png");
+	m_actor4.handle = LoadGraph("./img/Chara/Yellow/playerbase/walk_front_001.png");
+	m_actor5.handle = LoadGraph("./img/Chara/Green/playerbase/walk_front_001.png");
 	//”ñ“¯Šú‚Å“Ç‚Ýž‚Ý
 	SetUseASyncLoadFlag(true);
 }
@@ -18,24 +28,24 @@ Loading::~Loading()
 
 void Loading::Update()
 {
-	if (GetASyncLoadNum() > 0)
+	//if (GetASyncLoadNum() > 0)
+	if (true)
 	{
-		m_loadingPos.x--;
-		if (m_loadingPos.x < 0)
-		{
-			m_loadingPos.x = Game::kScreenWidth / 2;
-		}
+		++m_animCountFrame;
 	}
 }
 
 void Loading::Draw()
 {
 	DrawFormatString(0, 30, 0xff00ff, "ˆ—” = %d", GetASyncLoadNum());
-	if (GetASyncLoadNum() > 0)
+	//if (GetASyncLoadNum() > 0)
+	if (true)
 	{
-		DrawRotaGraph(
-			m_loadingPos.x, m_loadingPos.y,
-			1.0, 0.0,
-			m_loadingHandle, false);
+		DrawGraph(0, 0, m_loadingHandle, false);
+		DrawGraph(100, 0, m_actor1.handle, true);
+		DrawGraph(120, 0, m_actor2.handle, true);
+		DrawGraph(140, 0, m_actor3.handle, true);
+		DrawGraph(160, 0, m_actor4.handle, true);
+		DrawGraph(180, 0, m_actor5.handle, true);
 	}
 }
--- ColorfulFighter2/Loading.h
+++ ColorfulFighter2/Loading.h
@@ -4,7 +4,33 @@ class Loading
 {
 private:
 	int m_loadingHandle;
-	Vector3 m_loadingPos;
+    struct Actor//歩いてる人
+    {
+        int handle;
+        int animIndex;	//アニメーションの番号を数える(1増える毎にアニメーションが進む)
+        int animNum;		//アニメーションの数
+        int oneAnimFrame;	//1枚のアニメーションにかかるフレーム
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        /// <param name="handle">ハンドル</param>
+        /// <param name="animNum">絵の枚数</param>
+        Actor(int Handle, int AnimNum, int OneAnimFrame) :
+            handle(Handle),
+            animIndex(0),
+            animNum(AnimNum),
+            oneAnimFrame(OneAnimFrame)
+        {
+        }
+    };
+    //歩いてる人
+    Actor m_actor1;
+    Actor m_actor2;
+    Actor m_actor3;
+    Actor m_actor4;
+    Actor m_actor5;
+    //アニメーション関連
+    int m_animCountFrame;//戦ってるキャラクターのアニメーションに使う
 public:
 	Loading();
 	~Loading();
